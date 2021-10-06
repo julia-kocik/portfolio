05c581d8d2c5175ddd0324d2b6186b394fb1b03b
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-10-06 12:55:24 +0200

    Add Contact component

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { NavBar } from '../../views/NavBar/NavBar';
 import { Button } from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
@@ -16,7 +18,8 @@ const Component = ({className}) => (
       <NavBar className={styles.overlayOne}/>
     </div>
     <div className={styles.mainAbout}>
-      <div className={`${styles.overlayTwo} ${ styles.aboutContent}`}>
+      <div className={`${styles.overlayTwo} ${styles.aboutContent}`}>
+        <FontAwesomeIcon className={styles.icon} icon={faUser} />
         <p>ABOUT ME: I have experience in the scope of law and business, however, the discovery of my passion for programming completed the full spectrum of my professional interests. Since the beginning of the current year, I have been taking an intensive 9-month programming course and for over a year I have been devoting every available moment to develope my skills in this field. I am an ambitious person, confident in achieving my goals and I perceive every professional difficulty as an opportunity to acquire new skills and self-grow.</p>
         <p>STACK: HTML, CSS, RWD, Bootstrap, Git, JavaScript, AJAX, Node, React, Express, Redux, MongoDB, Jira, WebSocket, Mongoose, NPM, Webpack</p>
         <Button src="https://drive.google.com/file/d/1TLP1E_M3xhIlLa4zeu1fuUHNT65gI8kr/view" title="View Bootcamp Cerificate"/>
--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -2,21 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
+import { NavBar } from '../../views/NavBar/NavBar';
+import { Button } from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Contact.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Contact</h2>
-    {children}
+    <div className={styles.background}>
+      <NavBar className={styles.overlayOne}/>
+    </div>
+    <div className={styles.mainAbout}>
+      <div className={`${styles.overlayTwo} ${styles.aboutContent}`}>
+        <FontAwesomeIcon className={styles.icon} icon={faAddressBook} />
+        <Button src="https://github.com/julia-kocik" title="GitHub"/>
+        <Button src="https://www.linkedin.com/in/julia-kocik-942221216/" title="LinkedIn"/>
+        <Button src="" title="E-mail"/>
+      </div>
+    </div>
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
 };
 
